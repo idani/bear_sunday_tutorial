b097fadc9d1a10709e85d306b5168f62fbd36956
Shigehiro IDANI <idani@exp-t.com>
2019-11-08 00:14:53 +0900

    HtmlModuleを作成

---
+++ src/MyVendor.Weekday/src/Module/HtmlModule.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace MyVendor\Weekday\Module;
+
+use Madapaja\TwigModule\TwigErrorPageModule;
+use Madapaja\TwigModule\TwigModule;
+use Ray\Di\AbstractModule;
+
+class HtmlModule extends AbstractModule
+{
+	protected function configure()
+	{
+		$this->install(new TwigModule);
+		$this->install(new TwigErrorPageModule);
+	}
+}
