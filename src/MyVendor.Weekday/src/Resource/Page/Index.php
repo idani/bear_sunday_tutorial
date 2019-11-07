73a12e2d8407b4d6c1cdaa2b644ddc4d23125a30
Shigehiro IDANI <idani@exp-t.com>
2019-11-07 23:42:51 +0900

    onGetをHTTPで表示するために＠Embedに修正

--- src/MyVendor.Weekday/src/Resource/Page/Index.php
+++ src/MyVendor.Weekday/src/Resource/Page/Index.php
@@ -1,14 +1,25 @@
 <?php
 namespace MyVendor\Weekday\Resource\Page;
 
+use BEAR\Resource\Annotation\Embed;
 use BEAR\Resource\ResourceObject;
 
 class Index extends ResourceObject
 {
-    public function onGet(string $name = 'BEAR.Sunday') : ResourceObject
+    /**
+     * @Embed(rel="weekday", src="app://self/weekday{?year,month,day}")
+     *
+     * @param integer $year
+     * @param integer $month
+     * @param integer $day
+     * @return ResourceObject
+     */
+    public function onGet(int $year, int $month, int $day) : ResourceObject
     {
-        $this->body = [
-            'greeting' => 'Hello ' . $name
+        $this->body += [
+            'year' => $year,
+            'month' => $month,
+            'day' => $day,
         ];
 
         return $this;
