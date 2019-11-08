4788087619f07400dd8fa5857f719d812ee84db5
Shigehiro IDANI <idani@exp-t.com>
2019-11-08 13:24:05 +0900

    add mysql connection info

---
+++ src/Hirotae.Ticket/src/Module/TestModule.php
@@ -0,0 +1,25 @@
+<?php
+namespace Hirotae\Ticket\Module;
+
+use BEAR\Package\AbstractAppModule;
+use Ray\AuraSqlModule\AuraSqlModule;
+
+class TestModule extends AbstractAppModule
+{
+    /**
+     * {@inheritDoc}
+     *
+     * @return void
+     */
+    protected function configure()
+    {
+        $this->install(
+            new AuraSqlModule(
+                getenv('TKT_DB_DSN') . '_test',
+                getenv('TKT_DB_USER'),
+                getenv('TKT_DB_PASS'),
+                getenv('TKT_DB_SLAVE')
+            )
+        );
+    }
+}
