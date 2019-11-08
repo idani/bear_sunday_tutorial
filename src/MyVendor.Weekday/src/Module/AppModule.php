80e12f0a152690702987d94c4bea308470dd104a
Shigehiro IDANI <idani@exp-t.com>
2019-11-08 10:11:49 +0900

    REST API

--- src/MyVendor.Weekday/src/Module/AppModule.php
+++ src/MyVendor.Weekday/src/Module/AppModule.php
@@ -8,6 +8,7 @@ use MyVendor\Weekday\Annotation\BenchMark;
 use MyVendor\Weekday\Interceptor\BenchMarker;
 use MyVendor\Weekday\MyLogger;
 use MyVendor\Weekday\MyLoggerInterface;
+use Ray\CakeDbModule\CakeDbModule;
 
 class AppModule extends AbstractAppModule
 {
@@ -20,6 +21,13 @@ class AppModule extends AbstractAppModule
         require_once $appDir . '/env.php';
         $this->install(new AuraRouterModule($appDir . '/var/conf/aura.route.php'));
         $this->bind(MyLoggerInterface::class)->to(MyLogger::class);
+
+        $dbConfig = [
+            'driver' => 'Cake\Database\Driver\Sqlite',
+            'database' => $appDir . '/var/db/todo.sqlite3'
+        ];
+        $this->install(new CakeDbModule($dbConfig));
+
         $this->install(new PackageModule);
 
         $this->bindInterceptor(
---
+++ src/MyVendor.Weekday/src/Resource/App/Todos.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace MyVendor\Weekday\Resource\App;
+
+use BEAR\Package\Annotation\ReturnCreatedResource;
+use BEAR\RepositoryModule\Annotation\Cacheable;
+use BEAR\Resource\ResourceObject;
+use Ray\CakeDbModule\Annotation\Transactional;
+use Ray\CakeDbModule\DatabaseInject;
+
+/**
+ * @Cacheable
+ */
+class Todos extends ResourceObject
+{
+    use DatabaseInject;
+
+    public function onGet(int $id): ResourceObject
+    {
+        $this->body = $this
+            ->db
+            ->newQuery()
+            ->select('*')
+            ->from('todo')
+            ->where(['id' => $id])
+            ->execute()
+            ->fetch('assoc');
+
+        return $this;
+    }
+
+    /**
+     * @Transactional
+     * @ReturnCreatedResource
+     *
+     * @param string $todo
+     * @return ResourceObject
+     */
+    public function onPost(string $todo): ResourceObject
+    {
+        $statement = $this->db->insert(
+            'todo',
+            ['todo' => $todo, 'created_at' => new \DateTime('noew')],
+            ['created_at' => 'datetime']
+        );
+
+        $this->code = 201;
+        $id = $statement->lastInsertId();
+        $this->headers['Location'] = '/todos?id=' . $id;
+
+        return $this;
+    }
+
+    /**
+     * @Transactional
+     *
+     * @param integer $id
+     * @param string $todo
+     * @return ResourceObject
+     */
+    public function onPut(int $id, string $todo): ResourceObject
+    {
+        $this->db->update(
+            'todo',
+            ['todo' => $todo],
+            ['id' => $id]
+        );
+
+        $this->code = 204;
+
+        return $this;
+    }
+}
