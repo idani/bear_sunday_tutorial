08ff359429528c4f4628fd4995c2714724ffde47
Shigehiro IDANI <4g@idani.jp>
2019-11-11 20:50:05 +0900

    チュートリアルを再度実行

---
+++ src/Hirotae.Weekday/tests/Resource/App/WeekdayTest.php
@@ -0,0 +1,23 @@
+<?php
+namespace Hirotae\Weekday\Resource\App;
+
+use BEAR\Package\AppInjector;
+use BEAR\Resource\ResourceInterface;
+use PHPUnit\Framework\TestCase;
+
+class WeekdayTest extends TestCase
+{
+    private $resource;
+
+    protected function setUp()
+    {
+        $this->resource = (new AppInjector('Hirotae\Weekday', 'app'))->getInstance(ResourceInterface::class);
+    }
+
+    public function testOnGet()
+    {
+        $ro = $this->resource->get('app://self/weekday', ['year' => '2001', 'month' => '1', 'day' => '1']);
+        $this->assertSame(200, $ro->code);
+        $this->assertSame('Mon', $ro->body['weekday']);
+    }
+}
